39f69f6125d76ff3adc4f6738d4fc73d21c12ef7
F3rNnd4 <fernandaalves.tds2.senai@gmail.com>
2024-03-12 15:48:11 -0300

    primeiro commit

---
+++ ExercicioWhile.js
@@ -0,0 +1,18 @@
+//Peça dois números
+let inicio = 30;
+let fim = 35;
+
+//
+if (inicio <= fim) {
+    while(inicio <= fim){
+    console.log (inicio);
+    inicio++;
+    }
+}else{
+    while (inicio >= fim){
+    console.log (fim);
+    fim++
+    }
+}
+//Exibo que terminou
+console.log ("Terminou!");
